224d41ed12c96b926f8b8307686d8c42d3ece82e
Verneri Turunen <turunenv@outlook.com>
2023-06-15 09:04:19 +0300

    animate different passions in the main heading

--- scripts/main.js
+++ scripts/main.js
@@ -2,13 +2,25 @@
    an animation and requests that the browser calls a specific function
    to update an animation right before the next repaint */
 
-const title = document.querySelector(".main-title h1");
+const title = document.querySelector(".title-intro");
 const cursor = document.querySelector(".cursor");
+const passion = document.querySelector(".title-passion");
 
 let start, prevTimeStamp;
 let pauseStart;
 
-let titleText = "For the passion of programming.";
+let titleIntroDone = false;
+let passionListIdx = 0;
+let erasing = false;
+
+const titleText = "For the passion of ";
+const passionList = [
+  "programming",
+  "mathematics",
+  "creating",
+  "reading",
+  "learning",
+];
 
 let nextWaitTime;
 
@@ -25,16 +37,73 @@ function updateMainTitle(timeStamp) {
 
     cursor.classList.remove("flickering");
 
-    title.textContent = titleText.slice(0, title.textContent.length + 1);
+    //write the intro part of the title
+    if (!titleIntroDone) {
+      if (title.textContent.length === titleText.length) {
+        titleIntroDone = true;
+      } else {
+        title.textContent = titleText.slice(0, title.textContent.length + 1);
+      }
+    } else {
+      //write each passion one by one, leave the last one on the screen
+      console.log(passionListIdx);
+      let passionText = passionList[passionListIdx];
+      let passionLen = passionList[passionListIdx].length;
+      let currentLen = passion.textContent.length;
 
-    //stop condition
-    if (title.textContent.length === titleText.length) {
-      cursor.classList.add("flickering");
-      setTimeout(() => {
-        cursor.style.display = "none";
-      }, 2000);
-      return;
+      console.log(`currentLen: ${currentLen}, passionLen: ${passionLen}`);
+
+      if (currentLen < passionLen) {
+        if (!erasing) {
+          passion.textContent = passionText.slice(
+            0,
+            passion.textContent.length + 1
+          );
+        } else {
+          if (passion.textContent.length > 0) {
+            passion.textContent = passionText.slice(
+              0,
+              passion.textContent.length - 1
+            );
+          } else {
+            nextWaitTime = 500;
+            erasing = false;
+            cursor.classList.add("flickering");
+
+            console.log("increasint idx...");
+            passionListIdx++;
+          }
+        }
+      } else {
+        //wrote the hole word, time to backtrack
+        //unless it was the last passionList element, in which case return
+        if (passionListIdx === passionList.length - 1) {
+          cursor.classList.add("flickering");
+          setTimeout(() => {
+            cursor.style.display = "none";
+          }, 3000)
+
+          return;
+        }
+
+        if (!erasing) {
+          erasing = true;
+          cursor.classList.add("flickering");
+          nextWaitTime = 2000;
+        } else {
+          passion.textContent = passionText.slice(
+            0,
+            passion.textContent.length - 1
+          );
+        }
+      }
     }
+
+    // cursor.classList.add("flickering");
+    // setTimeout(() => {
+    //   cursor.style.display = "none";
+    // }, 2000);
+    // return;
 
     start = timeStamp;
   }
